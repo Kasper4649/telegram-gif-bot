ee3e12f56efb2a0ac6760022b697934c4b2383c9
Kasper4649 <imzxz123456@gmail.com>
2021-03-31 20:48:12 +0800

    [fix] fix webhook

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-from telegram.ext import Updater, CommandHandler
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 from config import TOKEN, WEBHOOK_HOST, PORT
 
-
-def start(update, context):
+def start_handler(update: Update, context: CallbackContext):
     update.message.reply_text("⛔")
 
-def help(update, context):
+def help_handler(update: Update, context: CallbackContext):
     update.message.reply_text("🚫")
 
 
 updater = Updater(TOKEN, use_context=True)
-# add handlers
+
 updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
                       url_path=TOKEN,
                       webhook_url=WEBHOOK_HOST)
-updater.dispatcher.add_handler(CommandHandler("start", start))
-updater.dispatcher.add_handler(CommandHandler("help", help))
+updater.dispatcher.add_handler(CommandHandler("start", start_handler))
+updater.dispatcher.add_handler(CommandHandler("help", help_handler))
+
+updater.start_polling()
 updater.idle()
