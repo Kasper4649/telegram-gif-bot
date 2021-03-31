963d11f4dbb815746c784419f2fc0f245af5bf5b
Kasper4649 <imzxz123456@gmail.com>
2021-03-31 21:04:54 +0800

    [fix] fix webhook

--- main.py
+++ main.py
@@ -8,15 +8,13 @@ def start_handler(update: Update, context: CallbackContext):
 def help_handler(update: Update, context: CallbackContext):
     update.message.reply_text("🚫")
 
-
 updater = Updater(TOKEN, use_context=True)
-
 updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
                       url_path=TOKEN,
-                      webhook_url=WEBHOOK_HOST)
+                      webhook_url=WEBHOOK_HOST + TOKEN)
+
 updater.dispatcher.add_handler(CommandHandler("start", start_handler))
 updater.dispatcher.add_handler(CommandHandler("help", help_handler))
 
 updater.start_polling()
-updater.idle()
